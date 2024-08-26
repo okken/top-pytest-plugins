79af34b404103ec92fbb56fb4a115174b8ea6884
Brian Okken <1568356+okken@users.noreply.github.com>
2024-08-26 09:20:48 -0700

    add summary. Thanks Jeff Triplett & Hugo for pointer to warehouse api

--- top_pytest.py
+++ top_pytest.py
@@ -7,6 +7,18 @@ def get_pytest_rows(json_data):
         if 'pytest' in project and 'pytest' != project:
             yield row
 
+from pprint import pprint
+
+def get_summary(project):
+    try:
+        r = httpx.get(f'https://pypi.org/pypi/{project}/json')
+        assert r.status_code == 200
+        data = r.json()
+        return data['info']['summary']
+
+    except AssertionError:
+        return r.status_code
+
 def main():
     data_source = 'https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json'
     r = httpx.get(data_source)
@@ -14,11 +26,14 @@ def main():
 
     data = json.loads(r.text)
     print(f"Data last updated: {data['last_update']} ")
-    print('| | Package | Downloads | Description |')
+    print('| # | Package | Downloads | Summary |')
     print('| -- | -- | -- | --- |')
     for i, row in enumerate(get_pytest_rows(data), start=1):
         url = f"https://pypi.org/project/{row['project']}"
-        print(f"| {i} | [{row['project']}]({url}) | {row['download_count']:,} |")
+        project = row['project']
+        count = row['download_count']
+        description = get_summary(project)
+        print(f"| {i} | [{project}]({url}) | {count:,} | {description} |")
 
 if __name__ == '__main__':
     main()
